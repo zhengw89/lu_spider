ba0c2a245a25487a5bd1b176b14a93b96b435db6
Zheng Wang <zhengw1989@hotmail.com>
2018-01-20 22:45:19 +0800

    完善详情

--- lu_detail.py
+++ lu_detail.py
@@ -1,31 +1,62 @@
 from urllib import request
 from bs4 import BeautifulSoup
+import pprint
 
 
 class LuFundDetail:
 
     def __init__(self, url):
         self.url = url
+        self._grade = []
 
         page = request.urlopen(url)
         soup = BeautifulSoup(page, 'html5lib')
 
         self.__base_info__(soup)
+        self.__grade__(soup)
 
     def __base_info__(self, soup):
         info_ul = soup.find('ul', {'class': 'fund-info'})
-        self.code = info_ul.contents[1].contents[1].string
-        self.name = info_ul.contents[3].contents[1].string
-        self.company = info_ul.contents[5].contents[1].string
-        self.type = info_ul.contents[7].contents[1].string
-        self.custodian = info_ul.contents[13].contents[1].string
-        self.setup_date = info_ul.contents[15].contents[1].string
-        self.scale = info_ul.contents[19].contents[1].string
+        self._code = info_ul.contents[1].contents[1].string
+        self._name = info_ul.contents[3].contents[1].string
+        self._company = info_ul.contents[5].contents[1].string
+        self._type = info_ul.contents[7].contents[1].string
+        self._custodian = info_ul.contents[13].contents[1].string
+        self._setup_date = info_ul.contents[15].contents[1].string.replace('年', '-').replace('月', '-').replace('日', '')
+        self._scale = info_ul.contents[19].contents[1].string
+
+        if self._scale.endswith('亿元'):
+            self._scale = int(float(self._scale.replace('亿元', '')) * 100000000)
+
+    def __grade__(self, soup):
+        grade_items = soup.find('div', {'class': 'fund-grade'}) \
+            .find('table', {'class': 'product-table'}) \
+            .tbody.find_all('tr')
+
+        for tr in grade_items:
+            if len(tr) > 3:
+                self._grade.append(LuFundGrade(tr))
+
+    def __repr__(self):
+        return str(self.__dict__.copy())
+
+    def pprint(self):
+        pprint.pprint(self.__dict__, width=1)
+
+
+class LuFundGrade:
+
+    def __init__(self, html):
+        self._organization = html.contents[1].contents[0].string
+        self._date = html.contents[3].contents[0].string
+        self._level = html.contents[5].contents[0]['class'][-1] \
+            .replace('one', '1').replace('two', '2') \
+            .replace('three', '3').replace('four', '4').replace('five', '5')
 
     def __repr__(self):
         return str(self.__dict__.copy())
 
 
 if __name__ == '__main__':
-    luDetail = LuFundDetail("https://e.lufunds.com/jijin/detail?productId=2279195")
-    print(luDetail.__repr__())
+    luDetail = LuFundDetail("https://e.lufunds.com/jijin/detail?productId=2708445")
+    luDetail.pprint()
